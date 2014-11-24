fcefa4b7192ecb23760c9767ed9b04e224cae013
U-MILKYWAY\hzhu <hzhu@FZTPFV1.olin.edu>
2014-11-24 16:30:27 -0600

    Grunt set up the important stuff.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name hdavidzhugithubioApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the hdavidzhugithubioApp
+ */
+angular.module('hdavidzhugithubioApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
