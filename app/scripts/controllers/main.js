358fc2bdef2bfa1028260cbf69dd2b5aefbc5df9
U-MILKYWAY\hzhu <hzhu@FZTPFV1.olin.edu>
2014-11-26 12:47:12 -0600

    Pushing over code that shouldn't be distributed.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -9,9 +9,17 @@
  */
 angular.module('hdavidzhugithubioApp')
   .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
+    // $scope.todos = ['Item 1', 'Item 2', 'Item 3', 'Item 4'];
+    $scope.todos = [];
+
+    $scope.addTodo = function () {
+  		$scope.todos.push($scope.todo);
+  		$scope.todo = '';
+		};
+
+		$scope.removeTodo = function (index) {
+  		$scope.todos.splice(index, 1);
+		};
+
   });
+
